848c44940da8f1e6f96c6a372bde80fe21296ae5
Kosta <kosta@headstudios.com.au>
2022-06-06 21:28:57 +1000

    Created code for first run of permalink loop

--- airtable-looptest.js
+++ airtable-looptest.js
@@ -3,22 +3,45 @@ This is a proof of concept of firstly looping through a table's view and populat
 row based on the value of another column (just created a new column called 'LoopDeLoop' for this purpose).
 
 */
-let WPtable = base.getTable("New WP");
-let view = WPtable.getView("All");
-let query = await view.selectRecordsAsync({fields: ["Title", "Name", "Contact", "Content"]});
-console.log(query);
+let SMStable = base.getTable("SMS Track");
+let view = SMStable.getView("All");
+let query = await view.selectRecordsAsync({fields: ["Permalink","Contact"]});
+
+//console.log(query);
 
 // print ID & "Priority" from each record:
 for (let record of query.records) {
-    console.log(`
-**${record.id}**
-${record.getCellValueAsString("Contact")}
-`);
-let contactShine = record.getCellValueAsString("Contact");
-let titleShine = record.getCellValueAsString("Name");
-let totalShine = contactShine + ' ' + titleShine;
-WPtable.updateRecordAsync(record, {
-    'Shortlink': totalShine
-})
+
+    /* 
+    This function will pass the permalink and have a shortlink returned, in the future ideally it would be good
+    to pass an array based on query and have it processed... however I'm not sure how to do this right now
+    so I will do it the old fashioned way. 
+    */
+
+    let permalink = record.getCellValue("Permalink");
+    let contact = record.getCellValueAsString("Contact");
+    contactns = contact.replace(/ /g,''); // this removes empty spaces from contact, renamed to contactns in case JS acts weird if I keep it the same name, which it probably won't
+
+    const data = { contact: contactns, permalink: permalink };
+
+    console.log(JSON.stringify(data));
+
+    let response = await remoteFetchAsync('https://staging2.conveyancingqld.com/pickandchoose.php', {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization' : 'Basic d2h5d29udHlvdWNvbm5lY3Q6Y2N1WSAxWWhkIDdsTmkgMzdWSSB5MVlyIDYySEs='
+    },
+    });
+
+    let body = await response.json();
+    console.log(body);
+
+    SMStable.updateRecordAsync(record, {
+    "Short Link": body.shortlink
+})        
+
+
 }
 
