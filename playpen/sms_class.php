2dc8006ace1844da5f7cdd88a692c6e0b8a3da66
Kosta <kosta@headstudios.com.au>
2022-06-29 10:47:01 +1000

    SMS Gateway Created

--- playpen/sms_class.php
+++ playpen/sms_class.php
@@ -8,6 +8,14 @@ $json = '[{"action":"smssender"},{"ID":"recpuIDwtkOxhYyLO","Name":"Kosta  Kondra
 
     
 DB::$user = 'root';
+DB::$password = 'root';
+
+// ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
+// App Builder Password: password77
+// com.terminator1000.thepath
+// Firebase 
+// Server Key: 	AAAADkKDrDU:APA91bGvrrJwHz9Fj7I_xg_ivqcJOO8Kb_VeZsaXMJONv9X3iGfySjEoiR1fyD7RNAsheDvzfOFYjB5W99a4cky1xBFX2kv7S58KaBjyJMHSIJgFZfX4SpFvbJ2qUSenjKhFId1jwP4P
+// Sender ID: 61245467701
 
 $results = DB::query("SELECT * FROM tasks");
 var_dump($results);
