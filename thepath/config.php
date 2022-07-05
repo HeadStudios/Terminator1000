28e84039870e55f15446d3ffe2b619c0bd63e06a
Kosta <kosta@headstudios.com.au>
2022-07-05 15:57:34 +1000

    ThePath upload test again

---
+++ thepath/config.php
@@ -0,0 +1,10 @@
+<?php
+define('DB_SERVER', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'smsgateway');
+define('SERVER_KEY', 'AAAA4MNr8SQ:APA91bGJ5doi5srqmhwdv0jSSrtNmveoGGAiY3wp9YVdyE3hoIq63VL4d-sQPvf4xyT4jI6y6RzGUmiYerB1kwsVnOKn9wPiekEyRsZXGsX2ErwvZZfm_rcpsWk9GP0asdasdasdasd');
+define('SENDER_ID', '12312456524');
+define('TIMEZONE', 'UTC');
+define('APP_SECRET_KEY', 'Y^!6drK4$@@fGN*ghasdkodshjasFj');
+define('APP_SESSION_NAME', 'SMS_GATEWAY');
