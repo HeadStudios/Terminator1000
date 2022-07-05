bd31fc32e57d0f1cf5556e4ad672c8b6bf2fb78b
Kosta <kosta@headstudios.com.au>
2022-07-05 16:00:00 +1000

    Change local

--- thepath/config.php
+++ thepath/config.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_SERVER', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_PASS', 'password');
 define('DB_NAME', 'smsgateway');
 define('SERVER_KEY', 'AAAA4MNr8SQ:APA91bGJ5doi5srqmhwdv0jSSrtNmveoGGAiY3wp9YVdyE3hoIq63VL4d-sQPvf4xyT4jI6y6RzGUmiYerB1kwsVnOKn9wPiekEyRsZXGsX2ErwvZZfm_rcpsWk9GP0asdasdasdasd');
 define('SENDER_ID', '12312456524');
